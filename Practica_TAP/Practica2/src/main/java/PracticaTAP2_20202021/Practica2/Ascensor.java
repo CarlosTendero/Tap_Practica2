69c6c9d51013d96fce1b38b006cf3cf5bbae49f6
anekom <andreeiitaa100ggmail.com>
2021-01-04 17:35:25 +0100

    clase factory edificio + clases vacias que dependen de ella

---
+++ Practica_TAP/Practica2/src/main/java/PracticaTAP2_20202021/Practica2/Ascensor.java
@@ -0,0 +1,11 @@
+package PracticaTAP2_20202021.Practica2;
+
+public class Ascensor {
+	
+	private int pisoActual;
+	private boolean emergencia;
+	private State current;
+	
+	
+
+}
---
+++ Practica_TAP/Practica2/src/main/java/PracticaTAP2_20202021/Practica2/Edificio.java
@@ -0,0 +1,35 @@
+package PracticaTAP2_20202021.Practica2;
+
+public class Edificio {
+	
+	private Ascensor ascensor1;
+	private Ascensor ascensor2;
+	private Ascensor ascensor3;
+	private Planta planta0;
+	private Planta planta1;
+	private Planta planta2;
+	private Planta planta3;
+	private Planta planta4;
+	private Planta planta5;
+	private Planta planta6;
+	private PanelDeControl panelDeControl;
+	
+	
+	public void  edificio() {
+		ascensor1 = new Ascensor();
+		ascensor2 = new Ascensor();
+		ascensor3 = new Ascensor();
+		
+		planta0 = new Planta();
+		planta1 = new Planta();
+		planta2 = new Planta();
+		planta3 = new Planta();
+		planta4 = new Planta();
+		planta5 = new Planta();
+		planta6 = new Planta();
+		
+		panelDeControl = new PanelDeControl();
+		
+	}
+
+}
