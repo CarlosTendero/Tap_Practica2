abcc60d822a11fb387153aa7aa14631c3fa8b006
CarlosTendero <carlitosten96@gmail.com>
2021-01-11 19:01:37 +0100

    Odio mi puta vida

--- Practica_TAP/Practica2/src/main/java/Clases/Impl/AscensorImpl.java
+++ Practica_TAP/Practica2/src/main/java/Clases/Impl/AscensorImpl.java
@@ -7,9 +7,9 @@ import Clases.Planta;
 public interface AscensorImpl {
 
 	public  ArrayList<Integer> movimientoAscensor(Planta Destino);	
-	public void cambiarPiso(Planta Destino);
 	public void cambiarEstadoPuerta();
 	public void cambiarEstadoAscensor();
-	public void mostrarPlantaActual();
 	public void activarAlarma();
+	public void anyadirDestino(int destino);
+	public int calculoDistancia(int destino);
 }
