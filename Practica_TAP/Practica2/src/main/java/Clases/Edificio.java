0c2aaef6240db5743c99c50996f3972168b694f1
CarlosTendero <carlitosten96@gmail.com>
2021-01-13 19:38:35 +0100

    Actualizando proyecto

--- Practica_TAP/Practica2/src/main/java/Clases/Edificio.java
+++ Practica_TAP/Practica2/src/main/java/Clases/Edificio.java
@@ -16,18 +16,23 @@ public class Edificio implements Clases.Impl.EdificioImpl{
 	//Constructor
 	//--------------------------------------------------------------	
 	public Edificio() {
-		panelControl = new PanelControl(ascensores);
+		
 		for (int i = 0; i < 3; i++)
 			this.ascensores.add(new Ascensor(0, new Clases.AscensorPuertaState.ParadoCerrado(), "Parado Cerrado", i));
 		for	(int i = 0; i < 7; i++)
 			this.plantas.add(new Planta(ascensores,i));
+	
+		this.panelControl = new PanelControl(ascensores);
 		
 		//Añadimos los observers a los ascensores. Observers = Plantas + Panel de Control.
+		/*
 		for (int i = 0; i < 3; i++) {
-			this.ascensores.get(i).attach(panelControl);;
+			this.ascensores.get(i).attach(this.panelControl);
 			for(int j=0; j<plantas.size();j++)
-				this.ascensores.get(i).attach(plantas.get(j));
-		}
+				this.ascensores.get(i).attach(this.plantas.get(j));
+		 
+		} Lo soluciona otro
+		*/
 	}
 	//--------------------------------------------------------------
 	//Setters y getters
@@ -52,5 +57,13 @@ public class Edificio implements Clases.Impl.EdificioImpl{
 	public void setPlantas(ArrayList<Planta> plantas) {
 		this.plantas = plantas;
 	}
+	public PanelControl getPanelControl() {
+		return panelControl;
+	}
+	public void setPanelControl(PanelControl panelControl) {
+		this.panelControl = panelControl;
+	}
+	
+	
 	
 }
