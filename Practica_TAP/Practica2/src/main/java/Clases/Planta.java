bcdbf2b58159ebca754b98e0a3e322f00bd66c4e
BorjaGarciaLamas <59957318+BorjaGarciaLamas@users.noreply.github.com>
2021-01-13 14:51:59 +0100

    añadidos comentarios en clase ascensor, planta y edificio

--- Practica_TAP/Practica2/src/main/java/Clases/Planta.java
+++ Practica_TAP/Practica2/src/main/java/Clases/Planta.java
@@ -2,6 +2,8 @@ package Clases;
 
 public class Planta implements Clases.Impl.PlantaImpl{
 
+	//Deberíamos revisar esta clase
+	
 	@Override
 	public void llamarAscensor() {
 		// TODO Auto-generated method stub
