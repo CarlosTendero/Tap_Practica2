ee6ecadd8174b79ac5cb7bb6743ef18374689c95
IMoll <nachomollamoros@gmail.com>
2021-01-14 10:00:44 +0100

    Arreglando el git

--- Practica_TAP/Practica2/src/main/java/Clases/Impl/Observer.java
+++
@@ -1,9 +0,0 @@
-package Clases.Impl;
-
-import Clases.Ascensor;
-
-public abstract class Observer {
-
-	public  abstract void update(Ascensor ascensor);		
-	
-}
--- Practica_TAP/Practica2/src/main/java/Clases/Impl/PlantaImpl.java
+++
@@ -1,8 +0,0 @@
-package Clases.Impl;
-
-import Clases.Ascensor;
-
-public interface PlantaImpl {
-
-	public void llamarAscensor(Ascensor ascensor);	
-}
